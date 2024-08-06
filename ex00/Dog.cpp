d8a8b5baff516f14df21c7b2479c383760ac0c09
Rajkumar Pambhar <rajkumarpambhar770@gmail.com>
2024-08-06 15:27:20 +0200

    Implemented class Dog

---
+++ ex00/Dog.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpambhar <rpambhar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/06 15:21:16 by rpambhar          #+#    #+#             */
+/*   Updated: 2024/08/06 15:26:53 by rpambhar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Dog.hpp"
+
+Dog::Dog() : Animal("Dog") {
+	std::out << "Dog default constructor called" << std::endl;
+}
+
+Dog::Dog(const Dog &other) : Animal(other) {
+	std::out << "Dog copy constructor called" << std::endl;
+}
+
+Dog& Dog::operator=(const Dog &other) {
+	std::cout << "Dog assignment operator called" << std::endl;
+	Animal::operator=(other);
+	return (*this);
+}
+
+Dog::~Dog() {
+	std::cout << "Dog destructor called" << std::endl;
+}
+
+void Dog::makeSound(void) const {
+	std::cout << "Woof Woof!" << std::endl;
+}
