8f1f05abfe7426dd4e25a792ebfd9027758c922b
Rajkumar Pambhar <rajkumarpambhar770@gmail.com>
2024-08-07 17:40:38 +0200

    defined and implemented class Ice

---
+++ ex03/Ice.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Ice.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpambhar <rpambhar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/07 17:33:17 by rpambhar          #+#    #+#             */
+/*   Updated: 2024/08/07 17:37:03 by rpambhar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Ice.hpp"
+
+Ice::Ice() : AMateria("ice") {}
+
+Ice::Ice(const Ice& other) : AMateria(other) {}
+
+Ice& Ice::operator=(const Ice& other) {
+	AMateria::operator=(other);
+	return (*this);
+}
+
+Ice::~Ice() {}
+
+AMateria* Ice::clone() const {
+	return new Ice(*this);
+}
+
+void Ice::use(ICharacter& target) {
+	std::cout << "* shoots an ice bold at " << target.getName() << " *" << std::endl;
+}
