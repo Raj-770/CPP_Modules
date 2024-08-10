7e18b3390cb2c6e10540f1a4ab54d2285a931864
Rajkumar Pambhar <rajkumarpambhar770@gmail.com>
2024-08-10 11:29:14 +0200

    defined and implemented class PresidentialPardonForm

---
+++ ex02/PresidentialPardonForm.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PresidentialPardonForm.hpp                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpambhar <rpambhar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/10 11:21:11 by rpambhar          #+#    #+#             */
+/*   Updated: 2024/08/10 11:24:05 by rpambhar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRESIDENTIALPARDONFORM_HPP
+#define PRESIDENTIALPARDONFORM_HPP
+
+#include "AForm.hpp"
+
+class PresidentialPardonForm : public AForm {
+	private:
+		std::string target;
+
+	public:
+		PresidentialPardonForm(const std::string& target);
+		PresidentialPardonForm(const PresidentialPardonForm& other);
+		PresidentialPardonForm& operator=(const PresidentialPardonForm& other);
+		~PresidentialPardonForm();
+
+		void execute(const Bureaucrat &executor) const;
+};
+
+#endif
