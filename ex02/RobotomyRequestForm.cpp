1481fb5abbc1aec610c8ce5e1ddc8596f32313ab
Rajkumar Pambhar <rajkumarpambhar770@gmail.com>
2024-08-10 11:20:20 +0200

    defined and implemented class RobotomyRequestForm

---
+++ ex02/RobotomyRequestForm.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RobotomyRequestForm.cpp                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpambhar <rpambhar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/10 11:10:28 by rpambhar          #+#    #+#             */
+/*   Updated: 2024/08/10 11:19:18 by rpambhar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "RobotomyRequestForm.hpp"
+
+RobotomyRequestForm::RobotomyRequestForm(const std::string& target) : AForm("RobotomyRequestForm", 72, 45), target(target) {}
+
+RobotomyRequestForm::RobotomyRequestForm(const RobotomyRequestForm& other) : AFrom(other), target(other.target) {}
+
+RobotomyRequestForm& RobotomyRequestForm::operator=(const RobotomyRequestForm& other) {
+	if (this != &other) {
+		AForm::operator=(other);
+		target = other.target;
+	}
+	return (*this);
+}
+
+RobotomyRequestForm::~RobotomyRequestForm() {}
+
+void RobotomyRequestForm::execute(const Bureaucrat& bureaucrat) const {
+	checkExecutionRequirements(bureaucrat);
+
+	std::cout << "Zzzzzrrrrrrggggggghhhhh" << std::endl;
+	std::srand(time(NULL));
+	if (std::rand() % 2 == 0)
+		std::cout << "Robotomy on " << target << " successful!" << std::endl;
+	else
+		std::cout << "Robotomy on " << target << " failed!" << std::endl;
+}
---
+++ ex02/RobotomyRequestForm.hpp
@@ -0,0 +1,20 @@
+#ifndef ROBOTOMYREQUESTFORM_HPP
+#define ROBOTOMYREQUESTFORM_HPP
+
+#include "AForm.hpp"
+#include <ctime>
+
+class RobotomyRequestForm : public AForm {
+	private:
+		std::string target;
+
+	public:
+		RobotomyRequestForm(const std::string& target);
+		RobotomyRequestForm(const RobotomyRequestForm& other);
+		RobotomyRequestForm& operator=(const RobotomyRequestForm& other);
+		~RobotomyRequestForm();
+
+		void execute(const Bureaucrat &executor) const;
+}
+
+#endif
