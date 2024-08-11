6b22474b6fc730837dcfc8c0e181b9d387e0a0a2
Rajkumar Pambhar <rajkumarpambhar770@gmail.com>
2024-08-11 17:01:53 +0200

    Added more template function to test iter

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: rpambhar <rpambhar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/11 16:55:58 by rpambhar          #+#    #+#             */
-/*   Updated: 2024/08/11 16:58:10 by rpambhar         ###   ########.fr       */
+/*   Updated: 2024/08/11 17:01:32 by rpambhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,16 @@ template <typename T>
 void iter(T* array, size_t length, void (*func)(T&)) {
 	for (size_t i = 0; i < lenght; i++)
 		func(array[i]);
+}
+
+template <typename T>
+void printElemet(T element) {
+	std::cout << element << " ";
+}
+
+template <typename T>
+void increment(T element) {
+	element++;
 }
 
 #endif
