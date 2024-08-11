74bef9a8847b4e0465d052fc83b65052f8fd6d31
Rajkumar Pambhar <rajkumarpambhar770@gmail.com>
2024-08-11 16:45:56 +0200

    implemeted main to test template function and fixed small issues

---
+++ ex00/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpambhar <rpambhar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/11 16:41:44 by rpambhar          #+#    #+#             */
+/*   Updated: 2024/08/11 16:42:14 by rpambhar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+
+int main( void ) {
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: rpambhar <rpambhar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/11 16:37:32 by rpambhar          #+#    #+#             */
-/*   Updated: 2024/08/11 16:40:44 by rpambhar         ###   ########.fr       */
+/*   Updated: 2024/08/11 16:45:09 by rpambhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,17 @@
 
 #include <iostream>
 
-template <typename> T
+template <typename T>
 void swap(T& a, T& b) {
 	T temp = a;
 	a = b;
 	b = temp;
 }
 
-template <typename> T
+template <typename T>
 const T& min(const T& a, T& b) { return ((a < b) ? a : b); }
 
-template <typename> T
-const T& min(const T& a, T& b) { return ((a > b) ? a : b); }
+template <typename T>
+const T& max(const T& a, T& b) { return ((a > b) ? a : b); }
 
 #endif
