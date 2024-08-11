032b3e68c5535c01f7f62f9350604c6195a370de
Rajkumar Pambhar <rajkumarpambhar770@gmail.com>
2024-08-11 14:54:55 +0200

    Added comment

--- ex01/Serializer.cpp
+++ ex01/Serializer.cpp
@@ -6,7 +6,7 @@
 /*   By: rpambhar <rpambhar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/11 14:40:12 by rpambhar          #+#    #+#             */
-/*   Updated: 2024/08/11 14:45:07 by rpambhar         ###   ########.fr       */
+/*   Updated: 2024/08/11 14:54:42 by rpambhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,15 @@ Serializer& Serializer::operator=(const Serializer& other) { (void)other; return
 
 Serializer::~Serializer() {}
 
+/*
+reinterpret_cast is a type of casting operator in C++ that allows you to convert
+one pointer type to another, one integral type to a pointer, or a pointer to an
+integral type. This operator performs a low-level reinterpretation of the bit
+pattern of its operand. It does not change the value during the conversion, but
+rather it tells the compiler to treat the sequence of bits as if they were a
+different type.
+*/
+
 uintptr_t Serializer::serialize(Data* ptr) {
 	return (reinterpret_cast<uintptr_t>(ptr));
 }
@@ -27,4 +36,3 @@ uintptr_t Serializer::serialize(Data* ptr) {
 Data* Serializer::deserialize(uintptr_t raw) {
 	return (reinterpret_cast<Data*>(raw));
 }
-
