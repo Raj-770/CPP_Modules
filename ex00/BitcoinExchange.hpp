3da5247ee4e89fbf331b791eff324775cf319884
Rajkumar Pambhar <rajkumarpambhar770@gmail.com>
2024-08-14 13:02:33 +0200

    implemented class BitcoinExchange

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: rpambhar <rpambhar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 13:44:43 by rpambhar          #+#    #+#             */
-/*   Updated: 2024/08/13 14:25:21 by rpambhar         ###   ########.fr       */
+/*   Updated: 2024/08/13 17:42:50 by rpambhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,17 @@ class BitcoinExchange {
 	private:
 		std::map<std::string, double> exchangeRate;
 		bool isValidDate(const std::string& date);
-		std::pair<std::string, double> parseData(const std::string& data_line, char delimeter);
+		std::pair<std::string, double> parseData(const std::string& data_line, char delimiter);
 		std::map<std::string, double> readDatabase(const std::string& db_file);
 
 	public:
-		BitcoinExchange(std::string& fileName);
+		BitcoinExchange(const std::string& fileName);
 		BitcoinExchange(const BitcoinExchange& other);
 		BitcoinExchange& operator=(const BitcoinExchange& other);
 		~BitcoinExchange();
 
 		double getExchangeRate(const std::string& data_line);
-		std::pair<std::string, double> getExchnageVlaue(const std::string& data_line);
+		std::pair<double, double> getExchangeValue(const std::string& data_line);
 };
 
 #endif
