43e03cb7b9cb8985e362641017c5b3f1cab48a5b
Rajkumar Pambhar <rajkumarpambhar770@gmail.com>
2024-08-14 16:21:11 +0200

    Fixed small issues

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -6,11 +6,13 @@
 /*   By: rpambhar <rpambhar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 13:37:04 by rpambhar          #+#    #+#             */
-/*   Updated: 2024/08/14 14:28:26 by rpambhar         ###   ########.fr       */
+/*   Updated: 2024/08/14 16:20:19 by rpambhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
+#include <sstream>
+#include <iostream>
 
 RPN::RPN() {}
 
@@ -18,7 +20,7 @@ RPN::RPN(const RPN& other) {
 	*this = other;
 }
 
-RPN& RPN:operator=(const RPN& other) {
+RPN& RPN::operator=(const RPN& other) {
 	if (this != &other)
 		stack = other.stack;
 	return *this;
@@ -26,29 +28,28 @@ RPN& RPN:operator=(const RPN& other) {
 
 RPN::~RPN() {};
 
-int RPN::evaluate(std::string& expression) {
-	std::istringstream iss;
+int RPN::evaluate(const std::string& expression) {
+	std::istringstream iss(expression);
 	std::string token;
 
 	while (iss >> token) {
 		if (isdigit(token[0])) {
-			stack.push(stod(token[0]))
+			stack.push(std::stod(token));
 		} else if (token == "+" || token == "-" || token == "*" || token == "/") {
 			if (stack.size() < 2)
-				throw std::runtime_error("Error: insufficient values in expression")
+				throw std::runtime_error("Error: insufficient values in expression");
 			int second = stack.top(); stack.pop();
 			int first = stack.top(); stack.pop();
-
-			if (token == "+") push(first + second);
-			else if (token == "-") push(first - second);
-			else if (token == "*") push(first * second);
+			if (token == "+") stack.push(first + second);
+			else if (token == "-") stack.push(first - second);
+			else if (token == "*") stack.push(first * second);
 			else if (token == "/") {
-				if (second = 0)
+				if (second == 0)
 					throw std::runtime_error("Error: Division by 0");
-				push(first / second);
-			} else {
-				throw std::runtime_error("Invalid token!");
+				stack.push(first / second);
 			}
+		} else {
+			throw std::runtime_error("Error: Invalid token!");
 		}
 	}
 	if (stack.size() != 1) {
